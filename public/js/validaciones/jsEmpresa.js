e0b4994a8c20c81184179527e5803c6cabf1bb83
kevv28 <gc2006062@ues.edu.sv>
2024-11-12 23:21:24 -0600

    Ordenamiento de controlador sucursal

--- public/js/validaciones/jsEmpresa.js
+++ public/js/validaciones/jsEmpresa.js
@@ -1,22 +1,37 @@
 $(document).ready(function () {
-    // Evento para enviar el formulario `#sucursalForm`
-    $('#sucursalForm').submit(function (e) {
+    // Evento para enviar el formulario `#empresaForm`
+    $('#empresaForm').submit(function (e) {
         e.preventDefault();
+
         let form = $(this);
         let url = form.attr('action');
-        let method = $('#method').val();
+
+        // Crear un objeto FormData para enviar archivos y datos
+        let formData = new FormData(this);
 
         $.ajax({
             url: url,
-            method: method,
-            data: form.serialize(),
+            method: 'POST', // Usa POST con FormData para simulación de PUT
+            data: formData,
+            processData: false, // No procesar los datos
+            contentType: false, // No establecer el tipo de contenido
             success: function (response) {
                 Toast.fire({
                     icon: response.type,
                     title: response.message
                 });
-                $('#modalForm').modal('hide');
-                mostrarDatos();
+                $('#modalFormE').modal('hide');
+
+                // Actualiza los datos en la página sin recargar
+                let idEmpresa = response.data.idEmpresa;
+                $(`#empresa-nit-${idEmpresa}`).text(response.data.nit);
+                $(`#empresa-nombre-${idEmpresa}`).text(response.data.nombre);
+
+                // Actualizar la imagen sin recargar la página
+                if (response.data.logo) {
+                    // Actualiza el contenedor de la imagen con la nueva URL
+                    $(`#image-previewI-${idEmpresa}`).css('background-image', `url(/${response.data.logo})`);
+                }
             },
             error: function (xhr) {
                 if (xhr.status === 422) {
@@ -34,129 +49,48 @@ $(document).ready(function () {
         });
     });
 
-    // Evento para enviar el formulario `#confirmarForm`
-    $('#confirmarForm').submit(function (e) {
-        e.preventDefault();
-        let form = $(this);
-        let url = form.attr('action');
-        let method = $('#methodS').val();
-
-        $.ajax({
-            url: url,
-            method: method,
-            data: form.serialize(),
-            success: function (response) {
-                Toast.fire({
-                    icon: response.type,
-                    title: response.message
-                });
-                $('#modalConfirm').modal('hide');
-                if (response.type == 'success') {
-                    mostrarDatos();
-                }
-            },
-            error: function (xhr) {
-                Toast.fire({
-                    icon: 'error',
-                    title: 'Ocurrió un error. Por favor, inténtelo de nuevo.'
-                });
-            }
-        });
-    });
-
-
-    // Otros eventos para agregar, editar y eliminar sucursales
-    $('#btnAgregar').click(function () {
-        agregar();
-    });
-
-    $(document).on('click', '.btnEditar', function () {
-        editar($(this).data('id'));
-    });
-
-    $(document).on('click', '.btnEliminar', function () {
-        eliminar($(this).data('id'));
-    });
-
-    $(document).on('click', '.btnDeshabilitar', function () {
-        baja($(this).data('id'));
-    });
-
-    $(document).on('click', '.btnHabilitar', function () {
-        alta($(this).data('id'));
+    // Evento para abrir el modal de edición y cargar datos de la empresa seleccionada
+    $(document).on('click', '.btnEditarE', function () {
+        editarE($(this).data('id'));
     });
 });
 
-function agregar() {
-    const errorSpans = document.querySelectorAll('span.text-danger');
-    errorSpans.forEach(function (span) {
-        span.innerHTML = '';
-    });
-
-    $('#titulo').text("Nuevo Registro");
-    $('#telefono').val('');
-    $('#direccion').val('');
-    $('#ubicacion').val('');
-
-    $('#method').val('POST');
-    $('#sucursalForm').attr('action', '');
-    $('#modalForm').modal('show');
-}
-
-function editar(idSucursal) {
-    $.get('/opciones/empresa/' + idSucursal + '/edit', function (obj) {
+function editarE(idEmpresa) {
+    $.get('/opciones/empresa/' + idEmpresa + '/editEmpresa', function (empresa) {
+        // Limpia los errores previos
         const errorSpans = document.querySelectorAll('span.text-danger');
-        errorSpans.forEach(function (span) {
-            span.innerHTML = '';
-        });
+        errorSpans.forEach(span => span.innerHTML = '');
+
+        // Preparación de formulario
         $('#titulo').text("Editar Registro");
-        $('#telefono').val(obj.telefono);
-        $('#direccion').val(obj.direccion);
-        $('#ubicacion').val(obj.ubicacion);
+        $('#imagenTemp').val(empresa.logo);
+        $('#logo').val('');
 
-        $('#method').val('PUT');
-        $('#sucursalForm').attr('action', '/opciones/empresa/' + idSucursal);
-        $('#modalForm').modal('show');
-    });
-}
-
-function eliminar(idSucursal) {
-    $('#confirmarForm').attr('action', '/opciones/empresa/' + idSucursal);
-    $('#methodS').val('Delete');
-    $('#dialogo').text('Está a punto de eliminar permanentemente el registro. ¿Desea continuar?');
-}
-
-function baja(idSucursal) {
-    $('#confirmarForm').attr('action', '/opciones/empresa/baja/' + idSucursal);
-    $('#methodS').val('get');
-    $('#dialogo').text('Está a punto de deshabilitar el registro. ¿Desea continuar?');
-}
-
-function alta(idSucursal) {
-    $.ajax({
-        url: '/opciones/empresa/alta/' + idSucursal,
-        method: 'get',
-        success: function (response) {
-            Toast.fire({
-                icon: response.type,
-                title: response.message
-            });
-            if (response.type == 'success') {
-                mostrarDatos();
-            }
-        },
-        error: function (xhr) {
-            Toast.fire({
-                icon: 'error',
-                title: 'Ocurrió un error. Por favor, inténtelo de nuevo.'
-            });
+        // Verifica si existe un logo antes de intentar cargarlo
+        if (empresa.logo) {
+            $('#image-preview').css('background-image', 'url(/' + empresa.logo + ')');
+            $('#image-preview').css('background-size', 'cover');
+            $('#image-preview').css('background-position', 'center');
+            $('#iconContainer').hide();
+            $('#textImage').hide();
+        } else {
+            $('#image-preview').css('background-image', 'none');
+            $('#iconContainer').show();
+            $('#textImage').show();
         }
+
+        $('#nit').val(empresa.nit);
+        $('#nombre').val(empresa.nombre);
+        $('#method').val('PUT');
+        $('#empresaForm').attr('action', '/opciones/empresa/' + idEmpresa + '/updateEmpresa');
+        $('#modalFormE').modal('show');
     });
 }
 
-function mostrarDatos() {
+
+function mostrarDatosEmpresa() {
     $.ajax({
-        url: '/obtener-sucursales',
+        url: '/obtener-empresa', // Asegúrate de que esta ruta devuelva los datos de las empresas
         method: 'GET',
         dataType: 'json',
         success: function (data) {
@@ -164,22 +98,13 @@ function mostrarDatos() {
 
             originalData = data.map(c => {
                 let acciones;
-                if (c.estado == 1) {
+                if (c.estado === 1) {
                     acciones = `
-                        <a role="button" data-bs-toggle="modal" data-bs-target="#modalForm" data-id="${c.idSucursal}" data-bs-tt="tooltip" data-bs-original-title="Editar" class="btnEditar me-3">
+                        <a role="button" data-bs-toggle="modal" data-bs-target="#modalFormE" data-id="${c.idEmpresa}" data-bs-tt="tooltip" data-bs-original-title="Editar" class="btnEditarE me-3">
                             <i class="fas fa-pen text-secondary"></i>
                         </a>
-                        <a role="button" data-bs-toggle="modal" data-bs-target="#modalConfirm" data-id="${c.idSucursal}" data-bs-tt="tooltip" data-bs-original-title="Deshabilitar" class="btnDeshabilitar me-3">
+                        <a role="button" data-bs-toggle="modal" data-bs-target="#modalConfirm" data-id="${c.idEmpresa}" data-bs-tt="tooltip" data-bs-original-title="Deshabilitar" class="btnDeshabilitar me-3">
                             <i class="fas fa-minus-circle text-secondary"></i>
-                        </a>
-                    `;
-                } else {
-                    acciones = `
-                        <a role="button" data-id="${c.idSucursal}" data-bs-tt="tooltip" data-bs-original-title="Habilitar" class="btnHabilitar me-3">
-                            <i class="fas fa-arrow-up text-secondary"></i>
-                        </a>
-                        <a role="button" data-bs-toggle="modal" data-bs-target="#modalConfirm" data-id="${c.idSucursal}" data-bs-tt="tooltip" data-bs-original-title="Eliminar" class="btnEliminar me-3">
-                            <i class="fas fa-trash text-secondary"></i>
                         </a>
                     `;
                 }
@@ -191,20 +116,17 @@ function mostrarDatos() {
                         </div>
                     </td>
                     <td class="px-1">
-                        <p class="text-xs font-weight-bold mb-0">${c.idSucursal}</p>
+                        <p class="text-xs font-weight-bold mb-0">${c.idEmpresa}</p>
                     </td>
                     <td class="px-1">
-                        <p class="text-xs font-weight-bold mb-0">${c.telefono}</p>
+                        <p class="text-xs font-weight-bold mb-0">${c.nit}</p>
                     </td>
                     <td class="px-1">
-                        <p class="text-xs font-weight-bold mb-0">${c.direccion}</p>
-                    </td>
-                    <td class="px-1">
-                        <p class="text-xs font-weight-bold mb-0">${c.ubicacion}</p>
+                        <p class="text-xs font-weight-bold mb-0">${c.nombre}</p>
                     </td>
                     <td class="px-1 text-sm">
-                        <span class="badge badge-xs opacity-7 bg-${c.estado == 1 ? 'success' : 'secondary'}">
-                            ${c.estado == 1 ? 'activo' : 'inactivo'}
+                        <span class="badge badge-xs opacity-7 bg-${c.estado === 1 ? 'success' : 'secondary'}">
+                            ${c.estado === 1 ? 'activo' : 'inactivo'}
                         </span>
                     </td>
                     <td>
@@ -220,32 +142,18 @@ function mostrarDatos() {
         error: function (xhr) {
             Toast.fire({
                 icon: 'error',
-                title: 'Ocurrió un error al cargar las categorías.'
+                title: 'Ocurrió un error al cargar las empresas.'
             });
         }
     });
 }
 
-function validarInput(input) {
-    let telefonoValue = input.value;
-
-    // Eliminar caracteres no válidos
-    telefonoValue = telefonoValue.replace(/[^+\d\s-]/g, '');
-
-    // Eliminar guiones existentes
-    telefonoValue = telefonoValue.replace(/-/g, '');
-
-    // Limitar la longitud máxima a 14 caracteres
-    if (telefonoValue.length > 8) {
-        telefonoValue = telefonoValue.slice(0, 8);
-    }
-
-    // Agregar un guion después del cuarto dígito
-    if (telefonoValue.length >= 8) {
-        telefonoValue = telefonoValue.slice(0, 4) + '-' + telefonoValue.slice(4);
-    }
-
-    // Asignar el valor al campo de entrada
-    input.value = telefonoValue;
-
-}
+// Mostrar la imagen cargada al seleccionarla
+document.getElementById('logo').addEventListener('change', function(event) {
+    const reader = new FileReader();
+    reader.onload = function() {
+        document.getElementById('image-preview').style.backgroundImage = `url(${reader.result})`;
+        document.getElementById('iconContainer').style.display = 'none'; // Oculta el ícono de cámara
+    };
+    reader.readAsDataURL(event.target.files[0]);
+});
